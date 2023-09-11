dc27fbd6be33097a6fff7b0b6f5c4da120992430
Sourav Singh <108568582+SvshSingh@users.noreply.github.com>
2023-09-12 01:09:32 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Adya</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Sourav</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
            I develop something <br className="sm:block hidden" /> and something and something.
           </p>
